8c2dbad200705be9996ab3cf8a9b5d310a5529a6
Jonah DC <Jonah DC>
2016-05-31 18:16:15 +0800

    Modified Config section

--- smtd.js
+++ smtd.js
@@ -4,13 +4,72 @@
 
   } else if (module && typeof module.exports === 'object') {
     module.exports = fn(require('jQuery'));
+
   } else {
-    fn(root.jQuery);
+    root.SMTD = fn(root.jQuery);
+
   }
 })(this, function($){
 
-  
+  var SMTD = {};
+  SMTD.version = '1.0.0';
 
+  var Config = {
+    defaultUrl: 'https://www.smrt.co.kr/program/cyberStation/lineview.jsp'
+    lines: {
+      1: {
+        name: 'Line 1'
+      },
+      2: {
+        name: 'Line 2'
+      },
+      3: {
+        name: 'Line 3'
+      },
+      4: {
+        name: 'Line 4'
+      },
+      5: {
+        name: 'Line 5'
+      },
+      6: {
+        name: 'Line 6'
+      },
+      7: {
+        name: 'Line 7'
+      },
+      8: {
+        name: 'Line 8'
+      },
+      9: {
+        name: 'Line 9 '
+      },
+      kyung: {
+        name: 'Gyeongui Jungang Line'
+      },
+      chun: {
+        name: 'Gyeongchun Line'
+      },
+      bun: {
+        name: 'Bundang Line'
+      },
+      in: {
+        name: 'Incheon Line'
+      },
+      sinbun: {
+        name: 'SinBundang Line'
+      },
+      suin: {
+        name: 'Suin Line'
+      }
+
+
+    }
+  }
+
+  SMTD.config = Config;
+
+  
 
 
 });
